5faa5ffdb0a1583c73352e2f0e39c7eab5953eda
sunilkumar27 <sunilkumar27@gmail.com>
2025-06-09 14:13:24 +0530

    Fix: Updated application properties

--- src/main/java/com/example/employeemanagement/EmployeeManagementApplication.java
+++ src/main/java/com/example/employeemanagement/EmployeeManagementApplication.java
@@ -10,11 +10,6 @@ import jakarta.annotation.PostConstruct;
 @ComponentScan(basePackages = "com.example.employeemanagement")
 public class EmployeeManagementApplication {
 
-    static {
-        System.out.println(">> ENV CHECK: DATABASE_URL = " + System.getenv("DATABASE_URL"));
-        System.out.println(">> ENV CHECK: SPRING_PROFILES_ACTIVE = " + System.getenv("SPRING_PROFILES_ACTIVE"));
-    }
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
