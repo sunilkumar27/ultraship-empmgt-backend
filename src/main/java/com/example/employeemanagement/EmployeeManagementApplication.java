1276f00526a55069ccbc7d20a3ebe566c370faa3
sunilkumar27 <sunilkumar27@gmail.com>
2025-06-09 13:53:39 +0530

    Fix: Added post construct to debug the flyway issue

--- src/main/java/com/example/employeemanagement/EmployeeManagementApplication.java
+++ src/main/java/com/example/employeemanagement/EmployeeManagementApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.employeemanagement")
 public class EmployeeManagementApplication {
@@ -11,5 +13,10 @@ public class EmployeeManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
+
+	@PostConstruct
+    public void checkEnv() {
+        System.out.println(">> DATABASE_URL: " + System.getenv("DATABASE_URL"));
+    }
 
 }
